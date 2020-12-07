9e527a35db3c571e6d39ddeada42299eb39b0933
Zarecky <gorbencoas@gmail.com>
2020-12-07 17:08:58 +0300

    compleated work

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+import { useCallback, useEffect, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import { AUTH_ENDPOINT, CLIENT_ID, REDIRECT_URI } from './api';
+
+const VK = window.VK
 
 function App() {
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    VK.Observer.subscribe('auth.login', ({ session: { user } }) => {
+      setName(user.first_name + ' ' + user.last_name)
+    })
+
+    VK.init({
+      apiId: CLIENT_ID
+    });
+  }, [])
+
+  const handleLoginButton = useCallback(async () => {
+    VK.Auth.login()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleLoginButton}>Войти через VK</button>
+        {name && (
+          <h1>{name}</h1>
+        )}
       </header>
     </div>
   );
---
+++ src/api.js
@@ -0,0 +1,3 @@
+export const CLIENT_ID = 7690969
+export const REDIRECT_URI = 'http://localhost:3000'
+export const AUTH_ENDPOINT = 'https://oauth.vk.com/authorize'
